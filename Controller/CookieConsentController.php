f15f5a23da92c3db3444145bf6671ee90d5d3460
SentimoAi <cyril.ekoule@gmail.com>
2026-07-07 09:16:54 -0400

    fix: use Symfony route attribute

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
@@ -160,7 +160,7 @@ class CookieConsentController
     {
         $locale = $request->get('locale');
         if (empty($locale) === false) {
-            $this->translator->setLocale($locale);
+            $this->translator->setLocale($locale;
             $request->setLocale($locale);
         }
     }
